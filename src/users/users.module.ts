800b28b41e559aaaf195506ac27f4a001f458b7c
Kamil <kamilwronka7@gmail.com>
2022-03-17 19:56:06 +0100

    Removed wrong config

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,16 +1,7 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { configService } from 'src/config/config.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-
-const {
-  host: rabbitMQHost,
-  port: rabbitMQPort,
-  user: rabbitMQUser,
-  password: rabbitMQPassword,
-} = configService.getRabbitMQConfig();
 
 @Module({
   imports: [
