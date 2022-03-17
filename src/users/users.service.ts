28a4c13798f7111a6d6f745432a5c88800363a41
Kamil <kamilwronka7@gmail.com>
2022-03-17 21:26:01 +0100

    Fixed users service url

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,16 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { catchError, map, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs';
 
 import { CreateUserDto } from './dto/create-user.dto';
-import { AxiosError, AxiosResponse } from 'axios';
+
 @Injectable()
 export class UsersService {
   constructor(private httpService: HttpService) {}
 
   create(createUserData: CreateUserDto): any {
-    console.log('request sent');
-
     const response = this.httpService.post(
-      'http://communicator-users-service:4000/users/create/account',
+      'http://users-service:4000/users/create/account',
       createUserData,
     );
 
