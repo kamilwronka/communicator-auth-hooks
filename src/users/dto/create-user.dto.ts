5b82a523fec5129aa34113057f41826b05c1ede4
Kamil Wronka <contact@kamilwronka.com>
2022-11-24 20:01:32 +0100

    Changed create user validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,19 +1,6 @@
-import { Type } from 'class-transformer';
-import { IsEmail, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
-
-class Auth0UserData {
-  @IsNotEmpty()
-  @IsString()
-  user_id: string;
-
+import { IsEmail, IsNotEmpty } from 'class-validator';
+export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
-}
-
-export class CreateUserDto {
-  @IsNotEmpty()
-  @ValidateNested()
-  @Type(() => Auth0UserData)
-  user: Auth0UserData;
 }
